be422b9dc2e34847f651601f000723b29b746da2
Ihar Papou <igarok-91@mail.ru>
2018-03-02 16:26:06 +0300

    task6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    var bkt = [];
+    var i = 0;
+            while (i < bracketsConfig.length)
+    {
+        bkt[i] = bracketsConfig[i].join("");
+        i++;
+    }
+    var j;
+    var k = str.length;
+      while (i < k)
+    {
+        for (j = 0; j < bkt.length; j++)
+
+        {
+            if (str.indexOf(bkt[j]) >= 0)
+            {
+                str = str.slice(0, str.indexOf(bkt[j])) + str.slice(str.indexOf(bkt[j]) + 2);
+            }
+
+        }
+        i++;
+    }
+    if (str.length >= 1)
+    {
+        return false;
+    }
+
+    return true;
 }
